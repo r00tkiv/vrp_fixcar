2f5da65cb45042f6cb3001aa3a25dfb0c143be22
Good fute pizdee 3 <76971196+GoodYT11@users.noreply.github.com>
2021-01-04 22:14:48 +0200

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,31 @@
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP","vRP_fixcar")
+
+groups = {"fondator", "superadmin", "admin", "god", "mod"}
+
+function vRP.checkGroup(source)
+	local user_id = vRP.getUserId({source})
+
+	for k,v in ipairs(groups) do
+		if vRP.hasGroup({user_id,v}) then
+			return true
+		end
+	end
+	return false
+end
+
+AddEventHandler('chatMessage', function(source, name, msg)
+	if msg == "/fix" or msg == "/FIX" then
+		local hasPerm = vRP.checkGroup(source)
+		if hasPerm then
+			CancelEvent()
+			TriggerClientEvent("wk:deleteVehicle", source)
+		else 
+			CancelEvent()
+			vRPclient.notify(source,{"Comanda restrictionata!"})
+		end
+	end
+end)
